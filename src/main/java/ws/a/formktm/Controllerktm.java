833b5921069f4191aa374c81fd9a82c7f3b8e703
mauladinayp7 <mauladinayp@gmail.com>
2022-11-15 17:28:00 +0700

    commit terbaru aku

--- src/main/java/ws/a/formktm/Controllerktm.java
+++ src/main/java/ws/a/formktm/Controllerktm.java
@@ -7,6 +7,7 @@ package ws.a.formktm;
 import java.io.IOException;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,16 +19,27 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class Controllerktm {
-    @RequestMapping("/KTM")
-    @ResponseBody
+    @RequestMapping("/view")
     public String getData(@RequestParam("gambar") MultipartFile gambar,
                           @RequestParam("nama") String text,
                           @RequestParam("nim") String number,
                           @RequestParam("fakultas") String fakultas,
-                          @RequestParam("jurusan") String jurusan)
+                          @RequestParam("jurusan") String jurusan,
+                          Model paket )
             throws IOException
     {
         String blob = Base64.encodeBase64String(gambar.getBytes());
-        return text+ "<br/>" +  number + "<br/>" + fakultas + "<br/>" + jurusan +"<br><img src='data:image/jpeg;base64,"+blob+"  '/>";
+        
+        String isigambar = "data:image/*;base64, "+blob+" ";
+        
+        paket.addAttribute("gbr", isigambar);
+        paket.addAttribute("nama", text);
+        paket.addAttribute("nim", number);
+        paket.addAttribute("fakultas", fakultas);
+        paket.addAttribute("jurusan", jurusan);
+        
+        return "view";
+    
+    
     }
 }
